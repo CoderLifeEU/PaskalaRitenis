77845b0bb845a4d67294a4f7c60c4100afea7e4c
Limoz <limoz@inbox.lv>
2016-01-13 19:43:46 +0200

    Try undo code - Finished Contacts page implementation on both projects
    Conflicts:
    	AdminConsole/AdminConsole.csproj
    	AdminConsole/Views/Shared/_Layout.cshtml
    	PaskalaRitenis/Content/Site.css
    	PaskalaRitenis/PaskalaRitenis.csproj
    	PaskalaRitenis/Views/Home/Contact.cshtml
    	PaskalaRitenis/Views/Shared/_Layout.cshtml

--- PaskalaRitenis/App_Start/RouteConfig.cs
+++ PaskalaRitenis/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace PaskalaRitenis
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Contact",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ PaskalaRitenis/Models/Contact/IContactsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PaskalaRitenis.Models.Contact
+{
+    public interface IContactsRepository
+    {
+        IEnumerable<ContactsModel> GetContacts();       
+    }
+}
