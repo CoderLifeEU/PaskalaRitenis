2ead276f033daacb3c77627dab9023e208cbf132
vadims-andrukovichs <vadims.andrukovichs@gmail.com>
2016-01-13 23:42:58 +0200

    Finished News implementation

---
+++ PaskalaRitenis/Models/INewsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PaskalaRitenis.Models
+{
+    public interface INewsRepository
+    {
+        IEnumerable<NewsModel> GetNews();
+    }
+}
