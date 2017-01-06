f542193776e8fa6e3b47bfa1c7c9e96bec656e87
coderliefeeu <coderliefeeu@gmail.com>
2017-01-06 13:36:51 +0200

    Added Export to xls for students and teachers pages +Added NPOI lib for xls generation

--- PaskalaRitenis/Controllers/RegistrationController.cs
+++ PaskalaRitenis/Controllers/RegistrationController.cs
@@ -1,8 +1,16 @@
-﻿using PaskalaRitenis.Models;
+﻿using NPOI.HSSF.Model;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using PaskalaRitenis.CustomManagers;
+using PaskalaRitenis.Models;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -63,116 +71,81 @@ namespace PaskalaRitenis.Controllers
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult TeacherList()
+        public void ExportStudentList()
         {
-            var model = GetRegistrationList("Teacher");
-            return PartialView(model);
-        }
+            var data = _repository.GetRegistrations(0, 99999, "Student", "");
 
-        public ActionResult ExportStudentList()
-        {
-            var registrList = GetRegistrationList("Teacher");
-
-            var sw = new StreamWriter(new MemoryStream());
-
-            sw.WriteLine("\"Token\",\"Vards\",\"Uzvards\",\"Telefons\",\"Email\",\"Pilseta\",\"Skolas tips\",\"Skolas nosaukums\",\"IrIzveletaSkola\",\"Klase\",\"Skolotajs\",\"Kopnite\",\"KopnitesTips\"");
-
-            foreach (var line in registrList)
+            List<string> headers = new List<string>()
             {
-                sw.WriteLine(string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\",\"{6}\",\"{7}\",\"{8}\",\"{9}\",\"{10}\",\"{11}\",\"{12}\"",
-                    line.PersonCode,
-                    line.Name,
-                    line.Surname,
-                    line.Phone,
-                    line.Email,
-                    line.City,
-                    line.StudyType,
-                    line.School,
-                    //line.IsSelected,
-                    line.SelectedKlass,
-                    line.Advicer,
-                    line.PlaceRequired,
-                    line.PlaceRequiredType));
-            }
+                "Token",
+                "Vards",
+                "Uzvards",
+                "Telefons",
+                "Email",
+                "Pilseta",
+                "Skolas tips",
+                "Skolas nosaukums",
+                "IrIzveletaSkola",
+                "Klase",
+                "Kopnite",
+                "KopnitesTips",
+            };
 
-            sw.Flush();
-            sw.BaseStream.Seek(0, SeekOrigin.Begin);
 
-            return File(sw.BaseStream, "text/csv", "ExportedStudentList.csv");
+            string filename = "ExportedStudentList.xls";
+            Response.ContentType = "application/vnd.ms-excel";
+            Response.AddHeader("Content-Disposition", string.Format("attachment;filename={0}", filename));
+            Response.Clear();
+
+            XlsManager manager = new XlsManager();
+
+            HSSFWorkbook hssfworkbook = manager.CreateRegistrationXLS(headers, data);
+            MemoryStream file = new MemoryStream();
+            hssfworkbook.Write(file);
+
+            Response.BinaryWrite(file.GetBuffer());
+            Response.End();
+
         }
 
 
-        public ActionResult ExportTeacherList()
+
+        public void ExportTeacherList()
         {
-            var registrList = GetRegistrationList("Teacher");
+            var data = _repository.GetRegistrations(0, 99999, "Teacher", "");
 
-            var sw = new StreamWriter(new MemoryStream());
-
-            sw.WriteLine("\"Vards\",\"Uzvards\",\"Telefons\",\"Email\",\"Pilseta\",\"Skolas tips\",\"Skolas nosaukums\",\"IrIzveletaSkola\",\"Kopnite\",\"KopnitesTips\"");
-
-            foreach (var line in registrList)
+            List<string> headers = new List<string>()
             {
-                sw.WriteLine(string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\",\"{6}\",\"{7}\",\"{8}\",\"{9}\"",
-                    line.Name,
-                    line.Surname,
-                    line.Phone,
-                    line.Email,
-                    line.City,
-                    line.StudyType,
-                    line.School,
-                    //line.IsSelected,
-                    line.PlaceRequired,
-                    line.PlaceRequiredType));
-            }
+                "Token",
+                "Vards",
+                "Uzvards",
+                "Telefons",
+                "Email",
+                "Pilseta",
+                "Skolas tips",
+                "Skolas nosaukums",
+                "IrIzveletaSkola",
+                "Klase",
+                "Kopnite",
+                "KopnitesTips",
+            };
 
-            sw.Flush();
-            sw.BaseStream.Seek(0, SeekOrigin.Begin);
 
-            return File(sw.BaseStream, "text/csv", "ExportedTeacherList.csv");
+            string filename = "ExportedTeacherList.xls";
+            Response.ContentType = "application/vnd.ms-excel";
+            Response.AddHeader("Content-Disposition", string.Format("attachment;filename={0}", filename));
+            Response.Clear();
+
+            XlsManager manager = new XlsManager();
+
+            HSSFWorkbook hssfworkbook = manager.CreateRegistrationXLS(headers, data);
+            MemoryStream file = new MemoryStream();
+            hssfworkbook.Write(file);
+
+            Response.BinaryWrite(file.GetBuffer());
+            Response.End();
         }
 
-        private List<RegistrationModel> GetRegistrationList(string regType)
-        {
-
-            var regList = new List<RegistrationModel>();
-
-            var con = ConfigurationManager.ConnectionStrings["DataSourceConnectionString"].ToString();
-
-            using (SqlConnection myConnection = new SqlConnection(con))
-            {
-                string oString = "Select * from Registration where RegType=@regType";
-                SqlCommand oCmd = new SqlCommand(oString, myConnection);
-                oCmd.Parameters.AddWithValue("@regType", regType);
-                myConnection.Open();
-                using (SqlDataReader reader = oCmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var record = new RegistrationModel();
-
-                        //record.Id = long.Parse(reader["Id"].ToString());
-                        record.PersonCode = reader["RegCode"].ToString();
-                        record.Name = reader["Vards"].ToString();
-                        record.Surname = reader["Uzvards"].ToString();
-                        record.Phone = reader["Telefons"].ToString();
-                        record.Email = reader["Email"].ToString();
-                        record.City = reader["Pilseta"].ToString();
-                        /*record.StudyType = reader["SkolasTips"].ToString();
-                        record.School = reader["SkolasNosaukums"].ToString();
-                        record.IsSelected = reader["IrIzveletaSkola"].ToString();*/
-                        record.SelectedKlass = reader["SkolasKlase"].ToString();
-                        record.Advicer = reader["Skolotajs"].ToString();
-                        record.PlaceRequired = reader["Kopnite"].ToString();
-                        record.PlaceRequiredType = reader["KopnitesTips"].ToString();
-
-                        regList.Add(record);
-                    }
-
-                    myConnection.Close();
-                }
-            }
-
-            return regList;
-        }
+        
     }
 }
---
+++ PaskalaRitenis/CustomManagers/XlsManager.cs
@@ -0,0 +1,113 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using PaskalaRitenis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaskalaRitenis.CustomManagers
+{
+    public class XlsManager
+    {
+
+        HSSFWorkbook hssfworkbook;
+        ISheet sheet;
+        ICellStyle header;
+
+        public void CreateWorkbook()
+        {
+            hssfworkbook = new HSSFWorkbook();
+            sheet = hssfworkbook.CreateSheet();
+
+            header = hssfworkbook.CreateCellStyle();
+            header.WrapText = true;
+
+            IFont font = hssfworkbook.CreateFont();
+            font.IsBold = true;
+            header.SetFont(font);
+        }
+
+        public HSSFWorkbook CreateRegistrationXLS(List<string> headers,List<Registration> data)
+        {
+            CreateWorkbook();
+            AddHeader(headers);
+            AddRegistrations(data);
+
+            return hssfworkbook;
+        }
+
+        public void AddHeader(List<String> data)
+        {
+            var row = sheet.CreateRow(0);
+
+            for(int i=0;i<data.Count;i++)
+            {
+                var cell = row.CreateCell(i);
+                cell.CellStyle = header;
+                cell.SetCellValue(data.ElementAt(i));
+            }
+        }
+
+        public void AddRegistrations(List<Registration> data)
+        {
+            int rowIndex = 1;
+            for (int i = 0; i < data.Count; i++)
+            {
+                int cellIndex = 0;
+                var row = sheet.CreateRow(rowIndex);
+                var cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).RegCode);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).Vards);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).Uzvards);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).Telefons);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).Email);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).Pilseta);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).SkolasTips);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).SkolasNosaukums);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).IrIzveletaSkola);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).SkolasKlase);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).Kopnite);
+                cellIndex++;
+
+                cell = row.CreateCell(cellIndex);
+                cell.SetCellValue(data.ElementAt(i).KopnitesTips);
+                cellIndex++;
+
+                rowIndex++;
+            }
+        }
+
+
+    }
+}
